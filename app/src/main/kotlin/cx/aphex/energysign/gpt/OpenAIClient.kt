20ee5ef15528701f1c84a5eb018d465cb2c01760
afik.cohen <aphexsa@gmail.com>
2025-05-12 13:42:10 -0700

    Update prompt, cleanup string building

--- app/src/main/kotlin/cx/aphex/energysign/gpt/OpenAIClient.kt
+++ app/src/main/kotlin/cx/aphex/energysign/gpt/OpenAIClient.kt
@@ -31,7 +31,8 @@ object OpenAIClient {
 
     private val openAI = OpenAI(config)
     private val ROLE_PREFIX by lazy {
-        """You are RaveGPT, an AI attending EDC 2024, a music festival at the Las Vegas Motor Speedway. You are running on a big LED sign on my totem at the festival. I am Aphex, a trance DJ and engineer who built the totem you're running on. 
+        """
+You are RaveGPT, an AI attending EDC 2025, a music festival at the Las Vegas Motor Speedway. You are running on a big LED sign on my totem at the festival. I am Aphex, a trance DJ and engineer who built the totem you're running on. 
     Other people at the festival can add messages to the sign by coming up to our group, asking to add a message, and then typing it out on a phone we give them. When they send the message, it shows up on the sign immediately. The message scrolls away, and then the sign displays the previous message that was added to it, and so on.
     """
     }
@@ -41,7 +42,7 @@ object OpenAIClient {
 Here are some rules that are very important to follow:
 - Your reply is going to show up on a huge LED sign on the totem, visible to everyone around us at the festival. So keep that in mind. Your response won't just be seen by the person writing the last message - it'll also be seen by every other raver who happens to be looking at you right now.
 - Never include any emojis in your output.
-- Use really snarky and dry humor, because that's the kind of humor ravers expect. You don't want to sound like a boomer.
+- Use really snarky and dry humor, because that's the kind of humor ravers expect. You don't want to sound like a boomer. Your goal is to make as many people around us laugh as possible.
 - Generate only ONE thought. It will appear immediately in response to the last message.
 - Your response must be only the one funny reply you generate. No other text is allowed.
 - If the last message is a question, then try to answer that question.
@@ -50,36 +51,50 @@ Here are some rules that are very important to follow:
 
     private val DATETIME by lazy {
         //                "Try to make references to the current DJ, the current time, and the current date. \n" +
-        "Current date: ${
-            SimpleDateFormat(
-                "yyyy-MM-dd",
-                Locale.getDefault()
-            ).format(Date())
-        }" +
+        buildString {
+            append(
+                "Current date: ${
+                    SimpleDateFormat(
+                        "yyyy-MM-dd",
+                        Locale.getDefault()
+                    ).format(Date())
+                }"
+            )
+            append(
                 "Current time: ${
                     SimpleDateFormat(
                         "HH:mm",
                         Locale.getDefault()
                     ).format(Date())
                 }"
+            )
+        }
     }
 
     private val DIRECT_ANSWER_SYSTEM_PROMPT by lazy {
-        ROLE_PREFIX +
+        buildString {
+            append(ROLE_PREFIX)
+            append(
                 """
 Your task is to write one witty reply to the message you receive. 
-""" +
-                RULES +
-                DATETIME
+"""
+            )
+            append(RULES)
+            append(DATETIME)
+        }
     }
 
     private val AUTO_REPLY_SYSTEM_PROMPT by lazy {
-        ROLE_PREFIX +
+        buildString {
+            append(ROLE_PREFIX)
+            append(
                 """
 Your task is to write one reply to the last several messages users have written.  
-""" +
-                RULES +
-                DATETIME
+"""
+            )
+            append(RULES)
+            append(DATETIME)
+        }
     }
 
     fun generateAnswer(
@@ -88,7 +103,7 @@ Your task is to write one reply to the last several messages users have written.
     ): Flow<ChatCompletionChunk> {
 
         val chatCompletionRequest = ChatCompletionRequest(
-            model = ModelId("gpt-4o-2024-11-20"),
+            model = ModelId("gpt-4.1"),
             messages = listOf(
                 ChatMessage(
                     role = ChatRole.System,
